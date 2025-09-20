632563944297bab748db143b10e602d5f2541ed4
Vanishree0992 <vani0992@gmail.com>
2025-09-20 17:56:08 +0530

    backend deployment

--- backend/settings.py
+++ backend/settings.py
@@ -107,5 +107,5 @@ SIMPLE_JWT = {
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "https://yourfrontenddomain.com"
+    "https://auth-project-frontend-chi.vercel.app"
 ]
